e7b225853e3f30263c464dc6f251ad9eb8d7ac42
ekatsnelson <elisek222@gmail.com>
2022-10-14 05:06:04 +0000

    Square, SQ, and Random done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,34 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_results.html.erb" })
   end
 
+  def blank_square_root_form
+    render({ :template => "calculation_templates/square_root_form.html.erb" })
+  end
+
+  def calculate_square_root
+    @num_to_sq = params.fetch("num_to_sq").to_f
+    @sqed_num = @num_to_sq ** 0.5
+    render({ :template => "calculation_templates/square_root_results.html.erb" })
+  end
+
+  def blank_payment_form
+    render({ :template => "calculation_templates/payment_form.html.erb" })
+  end
+
+  def calculate_payment
+    @num = params.fetch("number").to_f
+    @square_of_num = @num ** 2
+    render({ :template => "calculation_templates/payment_results.html.erb" })
+  end
+
+  def blank_random_form
+    render({ :template => "calculation_templates/random_form.html.erb" })
+  end
+
   def calculate_random
     @lower = params.fetch("user_min").to_f
     @upper = params.fetch("user_max").to_f
     @result = rand(@lower..@upper)
     render({ :template => "calculation_templates/rand_results.html.erb" })
   end
-
 end
